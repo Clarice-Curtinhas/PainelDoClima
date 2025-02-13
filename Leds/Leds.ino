546b17bc7b4a67851dbd60f44d317506e66ac738
mateuslizardo <mateuslizardo@outlook.com>
2025-02-13 10:55:23 -0300

    merge leds e mqtt deu errado

--- Leds/Leds.ino
+++ Leds/Leds.ino
@@ -1,5 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <time.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 // Fitas com os dados da UFES
 #define PIN1 4   // Pino de dados da fita 1
@@ -25,8 +27,8 @@
 #define TEMPMAX_VIX 40
 #define UMDMIN 0
 #define UMDMAX 100
-#define QLDMIN_VIX definir
-#define QLDMAX_VIX definir
+#define QLDMIN_VIX 1
+#define QLDMAX_VIX 1
 
 #define MENOR -1
 #define MAIOR 1
@@ -59,8 +61,32 @@ float qualidadeAtual; //Receber do mqtt
 
 char opcao = TEMPERATURA;
 
+//Variáveis MQTT
+typedef struct {
+  float temp, umidade, qualidade;
+} tDados;
+
+const char* ssid = "PIC2-2.4G";
+const char* password = "engcomp@ufes";
+//Free mqtt server for testing
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+//Local MQTT server - Tested using mosquitto mqtt for windows and linux
+//const char* mqtt_server = "192.168.137.1";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
 void setup(){
     Serial.begin(9600);
+
+    // Inicializa o MQTT
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 
     // Inicializa o NeoPixel
     strip1.begin();
@@ -76,7 +102,7 @@ void setup(){
 
     // Garante que todos os LEDs começam apagados
     strip1.show();
-    strip2.show();#define NUMANIMACAO 5
+    strip2.show();
     strip3.show();
     strip4.show();
     strip5.show();
@@ -97,6 +123,12 @@ void loop(){
     tempAtual = 24.5;
     umidadeAtual = 75;
     qualidadeAtual = 68.7;
+
+    //MQTT
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
 
     for (int i = 0; i < NUMPIXELS; i++){
         defineCor(i, tempAtual, umidadeAtual, qualidadeAtual);
@@ -207,4 +239,138 @@ void stuffHapenned(){
   if(opcao == TEMPERATURA) opcao = UMIDADE;
   else if(opcao == UMIDADE) opcao = QUALIDADE;
   else if(opcao == QUALIDADE) opcao = TEMPERATURA;
+}
+
+
+//Funcoes do MQTT
+void setup_wifi() {
+
+  //delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("PainelDoClima", "hello world");
+      // ... and resubscribe
+      client.subscribe("PainelDoClima/InfoUfes");
+      client.subscribe("PainelDoClima/InfoVix");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 1 second");
+      // Wait 5 seconds before retrying
+      delay(1000);
+    }
+  }
+}
+
+tDados leDados(tDados d, char* message){
+  int i=0;
+  char temp[5], umidade[5], qualidade[5];
+ 
+ while(message[i]!=','){
+  if(message[i]==','){
+    temp[i] = '\0';
+    break;
+  }
+  temp[i] = message[i];
+  
+  i++;
+ }
+ 
+  i++;
+  int j=0;
+ while(message[i]!=','){
+  if(message[i]==','){
+    umidade[j] = '\0';
+    break;
+  }
+  umidade[j] = message[i];
+  i++;
+  j++;
+ }
+
+  i++;
+  int k=0;
+ while(message[i]!='\0'){
+  if(message[i]=='\0'){
+    qualidade[k] = '\0';
+    break;
+  }
+  qualidade[k] = message[i];
+  i++;
+  k++;
+ }
+
+ d.temp = atof(temp);
+ //Serial.println(d.temp);
+ d.umidade = atof(umidade);
+ //Serial.println(d.umidade);
+ d.qualidade = atof(qualidade);
+ //Serial.println(d.qualidade);
+ return d;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.println(topic);
+
+  // Converte os dados recebidos para String
+  char message[100] = "";
+
+  tDados dadosUfes, dadosVix;
+  for (int i = 0; i < length; i++) {
+    message[i] += (char)payload[i];
+  }
+
+  if (strcmp(topic, "PainelDoClima/InfoUfes") == 0) {
+    dadosUfes = leDados(dadosUfes, message);
+    Serial.print("Dados Ufes - Temp: ");
+    Serial.println(dadosUfes.temp);
+    Serial.print("Umidade: ");
+    Serial.println(dadosUfes.umidade);
+    Serial.print("Qualidade: ");
+    Serial.println(dadosUfes.qualidade);
+  } else if (strcmp(topic, "PainelDoClima/InfoVix") == 0) {
+    dadosVix = leDados(dadosVix, message);
+    Serial.print("Dados Vix - Temp: ");
+    Serial.println(dadosVix.temp);
+    Serial.print("Umidade: ");
+    Serial.println(dadosVix.umidade);
+    Serial.print("Qualidade: ");
+    Serial.println(dadosVix.qualidade);
+  } else {
+    Serial.println("Tópico desconhecido");
+  }
+
+  Serial.print("Mensagem: ");
+  Serial.println(message);
+  
 }
